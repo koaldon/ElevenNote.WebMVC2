13c33cd0bf4b78226dbfbd1ad4bfbe5ef93971b3
koaldon <kirstin.oaldon@gmail.com>
2020-06-24 14:51:20 -0400

    Added NoteDetail service

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -54,5 +54,13 @@ namespace ElevenNote.WebMVC.Controllers
             var service = new NoteService(userId);
             return service;
         }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
     }
 }
